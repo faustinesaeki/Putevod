4ead132efd1fb8ea18940d700e7a95edbe2f130f
Faust <156181830+faustinesaeki@users.noreply.github.com>
2025-03-23 20:04:11 +0300

    1

---
+++ src/main/java/com/putevod/Putevod/PutevodApplication.java
@@ -0,0 +1,13 @@
+package com.putevod.Putevod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PutevodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PutevodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/putevod/Putevod/PutevodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.putevod.Putevod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PutevodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
